9ce15a092d6e6d23fc2b5d8d836e5c4b2cd8663b
luiz-ortega <luizngust@hotmail.com>
2021-05-01 01:51:26 -0300

    custom map

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,13 +1,28 @@
 const service = require("./services");
 
+Array.prototype.meuMap = function (callback) {
+  const novoArrayMapeado = [];
+  for (let indice = 0; indice <= this.length - 1; indice++) {
+    const resultado = callback(this[indice], indice);
+    novoArrayMapeado.push(resultado);
+  }
+  return novoArrayMapeado;
+};
+
 async function main() {
   try {
     const results = await service.obterPessoas("a");
-    const names = [];
 
-    result.results.forEach(function (item) {
-      names.push(item.name);
-    });
+    // const names = [];
+    // results.results.forEach(function (item) {
+    //   names.push(item.name);
+    // });
+
+    // const names = results.results.map((pessoa) => pessoa.name);
+
+    const names = results.results.meuMap(
+      (pessoa, indice) => `[${indice}]${pessoa.name}`
+    );
 
     console.log("names", names);
   } catch (error) {
