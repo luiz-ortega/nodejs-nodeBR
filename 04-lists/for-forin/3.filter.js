d6e27814521f832c109740f61831f770d2228290
luiz-ortega <luizngust@hotmail.com>
2021-05-01 02:10:53 -0300

    custom filter

---
+++ 04-lists/for-forin/3.filter.js
@@ -0,0 +1,35 @@
+const { obterPessoas } = require("./services");
+
+Array.prototype.meuFilter = function (callback) {
+  const lista = [];
+  for (index in this) {
+    const item = this[index];
+    const result = callback(item, index, this);
+    if (!result) continue;
+    lista.push(item);
+  }
+  return lista;
+};
+
+async function main() {
+  try {
+    const results = await obterPessoas("a");
+
+    // const familiaLars = results.results.filter(function (item) {
+    //   const result = item.name.toLowerCase().indexOf(`lars`) !== -1;
+    //   return result;
+    // });
+    // const names = familiaLars.map((pessoa) => pessoa.name);
+    // console.log(names);
+
+    const familiaLars = results.results.meuFilter(
+      (item, index, lista) => item.name.toLowerCase().indexOf("lars") !== -1
+    );
+    const names = familiaLars.map((pessoa) => pessoa.name);
+    console.log(names);
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+main();
