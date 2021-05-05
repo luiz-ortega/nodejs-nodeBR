6a793387bee6b2116a798797e5803742b80833bd
luiz-ortega <luizngust@hotmail.com>
2021-05-04 23:47:11 -0300

    mongodb commands

---
+++ 09-mult-db-postgres-mongodb/src/db/strategies/postgres.js
@@ -0,0 +1,78 @@
+const ICrud = require("./interfaces/interfaceCrud");
+const Sequelize = require("sequelize");
+
+class Postgres extends ICrud {
+  constructor() {
+    super();
+    this.driver = null;
+    this._herois = null;
+  }
+
+  async isConnected() {
+    try {
+      await this._driver.authenticate();
+      return true;
+    } catch (error) {
+      console.log("Error", error);
+      return false;
+    }
+  }
+
+  async defineModel() {
+    this._herois = this._driver.define(
+      "herois",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        nome: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+        poder: {
+          type: Sequelize.STRING,
+          require: true,
+        },
+      },
+      {
+        tableName: "TB_HEROIS",
+        freezeTableName: false,
+        timestamps: false,
+      }
+    );
+    await this._herois.sync();
+  }
+
+  async create(item) {
+    const { dataValues } = await this._herois.create(item, { raw: true });
+    return dataValues;
+  }
+
+  async read(item = {}) {
+    return this._herois.findAll({ where: item, raw: true });
+  }
+
+  async update(id, item) {
+    return await this._herois.update(item, { where: { id: id } });
+  }
+
+  async delete(id) {
+    const query = id ? { id } : {};
+    return this._herois.destroy({ where: query });
+  }
+
+  async connect() {
+    this._driver = new Sequelize("HEROES", "username", "secretpassword", {
+      host: "localhost",
+      dialect: "postgres",
+      quoteIdentifiers: false,
+      operatorsAliases: 0,
+    });
+    await this.defineModel();
+  }
+}
+
+module.exports = Postgres;
