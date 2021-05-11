ea9700a33a2fa5ddf3508c8c965bea1fd946e6fb
luiz-ortega <luizngust@hotmail.com>
2021-05-10 23:16:01 -0300

    test: ensure post request returning id and success message

---
+++ 16-api-cadastrar/src/routes/heroRoutes.js
@@ -0,0 +1,83 @@
+const BaseRoute = require("./base/baseRoute");
+const Joi = require("joi");
+
+const failAction = (request, headers, error) => {
+  throw error;
+};
+
+class HeroRoutes extends BaseRoute {
+  constructor(db) {
+    super();
+    this.db = db;
+  }
+
+  list() {
+    return {
+      path: "/herois",
+      method: "GET",
+      options: {
+        validate: {
+          failAction,
+          query: Joi.object({
+            skip: Joi.number().integer().default(0),
+            limit: Joi.number().integer().default(10),
+            nome: Joi.string().min(3).max(100).default(""),
+          }),
+        },
+      },
+
+      handler: (request, headers) => {
+        try {
+          const { skip, limit, nome } = request.query;
+
+          const query = {
+            nome: {
+              $regex: `.*${nome}*.`,
+            },
+          };
+
+          return this.db.read(nome ? query : {}, skip, limit);
+        } catch (error) {
+          console.log("Error", error);
+          return "Erro interno no servidor";
+        }
+      },
+    };
+  }
+
+  create() {
+    return {
+      path: "/herois",
+      method: "POST",
+      options: {
+        validate: {
+          failAction,
+          payload: Joi.object({
+            nome: Joi.string().required().min(3).max(100),
+            poder: Joi.string().required().min(2).max(100),
+          }),
+        },
+      },
+      handler: async (request) => {
+        try {
+          const { nome, poder } = request.payload;
+
+          const result = await this.db.create({
+            nome,
+            poder,
+          });
+
+          return {
+            message: "Heroi cadastrado com sucesso!",
+            _id: result._id,
+          };
+        } catch (error) {
+          console.log(error);
+          return "Internal error";
+        }
+      },
+    };
+  }
+}
+
+module.exports = HeroRoutes;
